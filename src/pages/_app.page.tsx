2e6d9ff78befcdc96d13b6efd9b4858256675a30
Will <willcx30@gmail.com>
2022-05-21 10:40:35 -0700

    fix

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -3,21 +3,20 @@ import type { AppProps } from 'next/app'
 import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
-
-  if (!process.env.GA_ID) return <Component {...pageProps} />
   
   const GA1 = `https://www.googletagmanager.com/gtag/js?id=${process.env.GA_ID}`;
+  
   const GA2 = `
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
     gtag('config', ${process.env.GA_ID});
   `;
-
+  
   return (
     <>
-      <Script async src={GA1} />
-      <Script id="ga2">{GA2}</Script>
+      <Script strategy="afterInteractive" src={GA1} />
+      <Script id="google-analytics" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: GA2 }}/>
       <Component {...pageProps} />
     </>
   )
